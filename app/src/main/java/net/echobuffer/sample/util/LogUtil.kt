6b00ec5ccec93cdd6eebd5f7306ed58366c6ffcc
Vincent Zhong <itvincent@gmail.com>
2019-01-04 20:11:45 +0800

    create project

---
+++ app/src/main/java/net/echobuffer/sample/util/LogUtil.kt
@@ -0,0 +1,22 @@
+package net.echobuffer.sample.util
+
+import android.content.Context
+import android.util.Log
+import android.widget.Toast
+
+/**
+ * Created by zhongyongsheng on 2018/9/13.
+ */
+const val TAG = "LogUtil"
+
+fun debugLog(msg: String) {
+    Log.i(TAG, "[${Thread.currentThread().name}] $msg")
+}
+
+fun errorLog(message: String, throwable: Throwable) {
+    Log.e(TAG, "[${Thread.currentThread().name}] ${message}", throwable)
+}
+
+fun showToast(context: Context, text: String) {
+    Toast.makeText(context.applicationContext, text, Toast.LENGTH_SHORT).show()
+}
---
+++ app/src/main/java/net/echobuffer/sample/util/LogView.kt
@@ -0,0 +1,46 @@
+package net.echobuffer.sample.util
+
+import android.content.Context
+import android.os.Looper
+import android.util.AttributeSet
+import android.util.Log
+import android.widget.ScrollView
+import android.widget.TextView
+import java.text.SimpleDateFormat
+import java.util.*
+
+/**
+ * 日志调试view
+ * Created by zhongyongsheng on 2018/11/23.
+ */
+class LogView @JvmOverloads constructor(
+        context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : ScrollView(context, attrs, defStyleAttr) {
+
+    val format = SimpleDateFormat("HH:mm:ss,SSS")
+    val textView = TextView(context)
+
+    //构造函数初始化，包含读取attrs和LayoutInflater
+    init {
+        textView.textSize = 10.0f
+        addView(textView)
+    }
+
+    fun log(msg: String) {
+        val currentThread = Thread.currentThread()
+        if (Looper.getMainLooper().thread == currentThread) {
+            mainLog(msg, currentThread)
+        } else {
+            post { mainLog(msg, currentThread) }
+        }
+
+    }
+
+    private fun mainLog(msg: String, currentThread: Thread) {
+        val line = "[${currentTime()}] $msg [${currentThread.name}]"
+        textView.text = "${textView.text}\n$line"
+        Log.i("LogView", line)
+    }
+
+    fun currentTime() = format.format(Date())
+}
