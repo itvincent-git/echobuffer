8b3af142ffd8fbaea6e92b5a4988e772bb4405b8
Vincent Zhong <itvincent@gmail.com>
2019-01-17 17:34:10 +0800

    add test

--- app/src/main/java/net/echobuffer/sample/PerformanceActivity.kt
+++ app/src/main/java/net/echobuffer/sample/PerformanceActivity.kt
@@ -2,6 +2,7 @@ package net.echobuffer.sample
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_performance.*
 import kotlinx.coroutines.*
 import net.echobuffer.EchoBuffer
 import net.echobuffer.RequestDelegate
@@ -18,7 +19,7 @@ class PerformanceActivity : AppCompatActivity(), CoroutineScope {
     private val echoBufferRequest = EchoBuffer.createRequest(object: RequestDelegate<Long, UserInfo> {
         override suspend fun request(data: Set<Long>): Map<Long, UserInfo> {
             debugLog("createRequest is $data")
-            delay(100)
+            delay(300)
             val map = mutableMapOf<Long, UserInfo>()
             for (item in data) {
                 map[item] = UserInfo(item, "$item name")
@@ -31,20 +32,45 @@ class PerformanceActivity : AppCompatActivity(), CoroutineScope {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_performance)
 
-        val randomCeil = 10000L
-        val random = Random(System.currentTimeMillis())
-        repeat(10000) {
-            val key = random.nextLong(randomCeil)
-            val call = echoBufferRequest.send(key)
-            debugLog("enqueueAwait $key")
-            launch {
-                try {
-                    withTimeout(2000) {
-                        val userInfo = call.enqueueAwait()
-                        debugLog("enqueueAwait response is $userInfo")
+        repeat_btn.setOnClickListener {
+            val randomCeil = 10000L
+            val random = Random(System.currentTimeMillis())
+            repeat(10000) {
+                val key = random.nextLong(randomCeil)
+                val call = echoBufferRequest.send(key)
+                debugLog("enqueueAwait $key")
+                launch {
+                    try {
+                        withTimeout(2000) {
+                            val userInfo = call.enqueueAwait()
+                            debugLog("enqueueAwait response is $userInfo")
+                        }
+                    } catch (t: Throwable) {
+                        errorLog("enqueueAwait response error $key", t)
                     }
-                } catch (t: Throwable) {
-                    errorLog("enqueueAwait response error $key", t)
+                }
+            }
+        }
+
+        res_btn.setOnClickListener {
+            val randomCeil = 1000L
+            val random = Random(System.currentTimeMillis())
+            repeat(10000) {
+                val key = random.nextLong(randomCeil)
+                launch {
+                    delay(it * 500L)
+                    try {
+                        //withTimeout(2000) {
+                            val call = echoBufferRequest.send(key)
+                            debugLog("test2 key:$key")
+                            val userInfo = call.enqueueAwait()
+                            withContext(Dispatchers.Main) {
+                                debugLog("test2 response is $userInfo")
+                            }
+                        //}
+                    } catch (t: Throwable) {
+                        errorLog("test2 response error $key", t)
+                    }
                 }
             }
         }
