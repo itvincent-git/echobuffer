afab166bb3cb27f7d2339c5fb8e322b33210ffa4
zhongyongsheng <zhongyongsheng@yy.com>
2020-03-23 11:57:13 +0800

    判断value是否为null，为null则remove

--- lib/src/main/java/net/echobuffer/Cache.kt
+++ lib/src/main/java/net/echobuffer/Cache.kt
@@ -10,24 +10,24 @@ import android.support.v4.util.LruCache
 interface Cache<K, V> {
     fun put(key: K, value: V?)
     operator fun get(key: K): V?
-    fun putAll(map: Map<K, V>)
+    fun putAll(map: Map<K, V?>)
 }
 
 /**
  * real implementation
  */
-class RealCache<K, V>(maxCacheSize: Int): Cache<K, V> {
+class RealCache<K, V>(maxCacheSize: Int) : Cache<K, V> {
     private val cache = LruCache<K, V>(maxCacheSize)
 
     override fun put(key: K, value: V?) {
-        cache.put(key, value)
+        value?.let { cache.put(key, it) } ?: cache.remove(key)
     }
 
     override fun get(key: K): V? {
         return cache.get(key)
     }
 
-    override fun putAll(map: Map<K, V>) {
+    override fun putAll(map: Map<K, V?>) {
         for (i in map) {
             put(i.key, i.value)
         }
